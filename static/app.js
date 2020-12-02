6de1bccd902012eee9c0512a96e07f37ad417132
siva <kumarsiva0707@gmail.com>
2020-12-02 19:27:57 +0530

    Search changes with pagination

--- static/app.js
+++ static/app.js
@@ -14,7 +14,7 @@ const Controller = {
     const table = document.getElementById("table-body");
     const rows = [];
     for (let result of results) {
-      rows.push(`<a href="/search?type=prev&idx=${result['prev']}"> prev</a> <tr>${result['data']}<tr/> <a href=/search?type=next&idx=${result['next']}> next</a>`);
+      rows.push(`<a href="/loadmore?type=prev&idx=${result['Prev']}"> prev</a> <tr>${result['Data']}<tr/> <a href=/loadmore?type=next&idx=${result['Next']}> next</a>`);
     }
     table.innerHTML = rows;
   },
